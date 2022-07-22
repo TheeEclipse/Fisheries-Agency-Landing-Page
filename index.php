36cc3b5c1212bcc68505a319233ee23bc65d11d0
Kelvin Ndungu Wanja <wanjakelvinndungu@gmail.com>
2022-07-22 12:53:42 +0300

    Update index.php

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Your Own Title</title>
+        <title>Astral Ecorp</title>
         <!-- CSS only -->
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
 <!-- JavaScript Bundle with Popper -->
@@ -18,24 +18,35 @@
 <link rel="stylesheet" href="styles.css">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
 </head>
 <body>
-<form method="POST" action="#">
-<div class="lit">
-    <a><img src="https://res.cloudinary.com/dyadjj49h/image/upload/v1654110104/elogo_mhfyg2.png" width="100px" height="100px" alt=""></a>
-    <ul>
+    <a><img src="https://res.cloudinary.com/dyadjj49h/image/upload/v1654110104/elogo_mhfyg2.png" width="100px" height="100px" alt="" style="margin-top:-12px;"></a>
+    <div class="float-lg-end">
+        <ul>
         <li><a href="https://astralecorp.com">CONTACT</a></li>
         <li><a href="https://astralecorp.com">GALLERY</a></li>
         <li><a href="https://astralecorp.com">MAPS</a></li>
         <li><a href="https://astralecorp.com">FLY FISHING</a></li>
         <li><a href="https://astralecorp.com">HOME</a></li>
-      </ul>
+    </ul>
+</div>
+<div class="float-end" id="container" onclick="myFunction(this)">
+    <div class="bar1"></div>
+    <div class="bar2"></div>
+    <div class="bar3"></div>
+  </div>
+  
+  <script>
+  function myFunction(x) {
+    x.classList.toggle("change");
+  }
+  </script>
+<form method="POST" action="#">
     <div class="rd">
         <h>Ready for this Year</h>
         <h2>FLY FISHING PACKAGE</h2>
     </div> 
-    <div class="bx">
         <table id="tb">
         <tr>
             <th>LOCATION</th>
@@ -45,13 +56,13 @@
         </tr>
         <tr>
             <td>
-                <select name="cars" id="places" style="width: 70px;">
-                    <option value="loc">Location</option>
-                    <option value="nep">Nepal</option>
-                    <option value="mum">Mumbai</option>
-                    <option value="cub">Cuba</option>
-                    <option value="oh">Ohaio</option>
-                    <option value="nai">Nairobi</option>
+                <select name="cars" id="cars" style="width: 70px;">
+                    <option value="volvo">Location</option>
+                    <option value="saab">Nepal</option>
+                    <option value="opel">Mumbai</option>
+                    <option value="audi">Cuba</option>
+                    <option value="audi">Ohaio</option>
+                    <option value="audi">Nairobi</option>
                   </select>
             </td>
             <td>
@@ -64,11 +75,17 @@
                 <button style="width: 70px; background-color: orangered;"><i class="fa fa-search" style="font-size:16px;color:whitesmoke">Search</i></button>
             </td>
         </tr>
+        <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td><button id="bt1">NEXT</button></td>
+        </tr>
     </table>
-    <button id="bt1">NEXT</button>
-    </div>
-</div>
+    
 </form>
+    </body>
+</html>
     <?php
      #handle errors here if anything is to be set as required and isn't satisfied by user
     
